cb583b33d8f337d7a219e232c395ad26c89893a7
Storm603 <93555141+Storm603@users.noreply.github.com>
2022-01-05 00:21:16 +0200

    Add files via upload

---
+++ T07. Hot Potato/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T07._Hot_Potato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split();
+
+            int jump = int.Parse(Console.ReadLine());
+
+            Queue<string> kids = new Queue<string>(names);
+
+            while (kids.Count > 1)
+            {
+                for (int i = 0; i < jump - 1; i++)
+                {
+                    kids.Enqueue(kids.Dequeue());
+                }
+
+                Console.WriteLine($"Removed {kids.Dequeue()}");
+            }
+
+            Console.WriteLine($"Last is {kids.Peek()}");
+        }
+    }
+}
---
+++ T08. Traffic Jam/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T08._Traffic_Jam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int carsPasingOnGreenCount = int.Parse(Console.ReadLine());
+
+            string car = string.Empty;
+
+            int crossedCars = 0;
+
+            Queue<string> carsQueue = new Queue<string>();
+
+            while ((car = Console.ReadLine()) != "end")
+            {
+                if (car == "green")
+                {
+                    for (int i = 0; i < carsPasingOnGreenCount; i++)
+                    {
+                        if (carsQueue.Count == 0)
+                        {
+                            break;
+                        }
+                        crossedCars++;
+                        Console.WriteLine($"{carsQueue.Dequeue()} passed!");
+                        
+                    }
+                }
+                else
+                {
+                    carsQueue.Enqueue(car);
+                }
+            }
+
+            Console.WriteLine($"{crossedCars} cars passed the crossroads.");
+        }
+    }
+}
